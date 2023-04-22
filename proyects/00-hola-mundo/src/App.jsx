151bbbba5180d1ec06af8a687b9a61d9d15da8c1
MateoEmilio1 <mateoyastor60@gmail.com>
2023-04-22 17:40:25 -0300

    acc

--- proyects/00-hola-mundo/src/App.jsx
+++ proyects/00-hola-mundo/src/App.jsx
@@ -3,19 +3,19 @@ import { TwitterFollowCard } from './TwitterFollowCard'
 
 export function App(){
 
-    const formatUserName =(userName) => `@${userName}`
+    const format =(userName) => `@${userName}`
     
     return(
     <section className="App">
         <TwitterFollowCard 
-            formatUserName={formatUserName} 
+            formatUserName={format} 
             isFollowing={true} 
             userName="mateoemilio1" 
             name="Mateo Emilio" />
 
 
         <TwitterFollowCard 
-            formatUserName={formatUserName}  //le pasamos la funcion directamente
+            formatUserName={format}  //le pasamos la funcion directamente
             isFollowing={false} 
             userName="midudev" 
             name="Miguel Angel Duran" />
