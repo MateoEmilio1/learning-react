35823d782deefcd320d3bfe99590910c1dcef07d
MateoEmilio1 <mateoyastor60@gmail.com>
2023-04-22 17:37:29 -0300

    update

---
+++ proyects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -0,0 +1,30 @@
+
+export function TwitterFollowCard({ formatUserName,userName, name, isFollowing }){
+    
+    return(
+        <article className='tw-followCard'>
+            <header className='tw-followCard-header'>
+                <img 
+                className='tw-followCard-avatar'
+                alt="El avatar de Tute" 
+                src={`https://unavatar.io/${userName}`} /> 
+
+                <div className='tw-followCard-info'>
+                    <strong>{name}</strong>
+                    <span className='tw-followCard-infoUsername'>{formatUserName(userName)}</span>
+                </div>
+            </header>
+
+            <aside>
+                <button className='tw-followCard-button'>
+                    Seguir
+                </button>
+
+            </aside>
+
+
+        </article>
+    )
+
+}
+
